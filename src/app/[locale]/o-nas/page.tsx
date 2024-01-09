f585df02059d49daa2b2c6274d1edab2d753ba35
m.liwen <liwen.marcin@gmail.com>
2024-01-10 00:35:47 +0100

    about pages imgs

--- src/app/[locale]/o-nas/page.tsx
+++ src/app/[locale]/o-nas/page.tsx
@@ -2,13 +2,15 @@ import Head from "next/head"
 import SecondHero from "@/app/componenets/secondHero"
 import AboutTitle from "@/app/componenets/about/aboutTitle"
 import Image from "next/image"
-import AboutUs from "@/../public/aboutUs.png"
+import AboutUs from "@/../public/Kalina02.png"
+import Marcin from "@/../public/Marcin01.png"
 import AboutBio from "@/app/componenets/about/aboutBio"
 import GabiinetFront from '@/../public/gabinet-front.png'
 import LinkMore from "@/app/componenets/linkMore"
 import { Link } from "@/navigation";
 import Slogan from "@/app/componenets/slogan"
-import { Metadata } from "next"
+import { Metadata } from "next";
+
 
 export const metadata: Metadata = {
     title: 'O nas',
@@ -26,11 +28,13 @@ export default function Onas() {
                     <AboutTitle />
                 </div>
             </section>
-            <section className="mb-16 py-20 bg-blue-100">
+            <section className="mb-16 py-2">
                 <div className="container">
-                    <div className="grid md:grid-cols-3 relative ">
-                        <div className="flex flex-col pr-8 mb-8 md:mb-0 self-center">
-                            <h2>Lokalizacja</h2>
+
+                    <div className="grid md:grid-cols-3 gap-8 relative ">
+
+                        <div className="flex flex-col pr-8 mb-8 md:mb-0 ">
+                            <h2 className='mb-14'>{"Lokalizacja"}</h2>
                             <p className="text-sm leading-8 "><strong>Gabinet Weterynaryjny w Zasiekach</strong> mieści się w budynku Apteki Arnika. Przed budynkiem jest wygodny parking dla klientów.</p>
 
 
@@ -43,21 +47,66 @@ export default function Onas() {
             </section>
             <section className="py-12  mb-16 ">
                 <div className="container relative z-10">
-                    <div className='grid md:grid-cols-3 border-b border-[#242424] mb-16'>
-                        <h2 className='mb-14'>{"Nasz zespół"}</h2>
-                    </div>
-                    <div className="grid md:grid-cols-3 relative">
-                        <AboutBio />
+                    <h2 className='mb-14'>{"Zespół"}</h2>
+                    <div className="grid md:grid-cols-3 gap-8 relative">
 
-                        <div>
+                        <article>
                             <Image
                                 src={AboutUs}
-                                width={515}
+                                width={360}
                                 height={611}
                                 alt="Kalina Adamkiewicz - Weterynarz"
+                                className=""
 
                             />
+                            <div className="mt-4">
+                                <strong className="">Kalina Adamkiewicz</strong>
+                                <p className="text-sm">Lekarz weterynarii, Chirurg</p>
+                                <p className="text-sm">Właściciel i Kierownik</p>
+                            </div>
+                        </article>
+                        <article>
+                            <Image
+                                src={Marcin}
+                                width={360}
+                                height={611}
+                                alt="Kalina Adamkiewicz - Weterynarz"
+                                className=""
+
+                            />
+                            <div className="mt-4">
+                                <strong className="">Marcin Liweń</strong>
+                                <p className="text-sm">Pomoc weterynaryjna</p>
+                                <p className="text-sm">Manager</p>
+                            </div>
+                        </article>
+                        {/*  <div className="md:col-span-2">
+                        
+                        <AboutBio />
+                        </div> */}
+
+
+                        <div>
+
                         </div>
+                    </div>
+
+                </div>
+            </section>
+            <section className="py-12  mb-16 ">
+                <div className="container relative z-10">
+                    <div className='grid md:grid-cols-3 border-b border-[#242424] mb-16'>
+                        <h2 className='mb-14'>{"Certyfikaty"}</h2>
+                    </div>
+                    <div className="grid md:grid-cols-3 gap-8 relative">
+                        {[1, 2, 3, 4, 5, 6].map(item => {
+                            return (
+                                <article className="p-4 bg-gray-100 rounded-xl min-h-[450px]">
+
+                                </article>
+                            )
+                        })}
+
                     </div>
 
                 </div>
--- src/app/componenets/about/aboutBio.tsx
+++ src/app/componenets/about/aboutBio.tsx
@@ -6,7 +6,7 @@ import { useTranslations } from 'next-intl';
 export default function AboutBio() {
     const h = useTranslations('HomePage')
     return ( 
-        <div className="flex flex-col pr-8 self-center col-span-2">
+        <div className="flex flex-col pr-8 ">
             <strong>Kalina Adamkiewicz</strong>
             <p className="max-w-2xl text-sm leading-8">{h('bio-1')}</p>
             <p className="max-w-2xl text-sm leading-8">{h('bio-2')}</p>
