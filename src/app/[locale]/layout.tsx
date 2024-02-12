aac8e1775581529a88bfc318ee4f7e28b338b5b6
m.liwen <liwen.marcin@gmail.com>
2024-02-13 00:02:28 +0100

    images

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -46,7 +46,10 @@ const localBussiness =
   "@context": "https://schema.org",
   "@type": "LocalBusiness",
   name: "Kalina Adamkiweicz | Gabinet Weterynaryjny",
-  "image": "",
+  "image": [
+    "https://www.kalinavet.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgabinet-front.182c3e95.png&w=3840&q=75",
+    "https://www.kalinavet.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FQACXa1M3TEKIGW5F0XST&w=1080&q=75"
+  ],
   "@id": "",
   url: "https://kalinavet.com",
   priceRange:"€zł",
