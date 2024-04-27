97c531ad47b6ff9f5bfa0124e33a223c36e6d835
m.liwen <liwen.marcin@gmail.com>
2024-04-28 00:54:04 +0200

    style mobile

--- src/app/componenets/header/index.tsx
+++ src/app/componenets/header/index.tsx
@@ -3,7 +3,7 @@ import HeaderNav from "./headerNav";
 
 export default function Header() {
     return (
-        <header className="fixed py-2 lg:py-0 lg:sticky left-0 right-0  top-0 z-50 bg-blue-500 ">
+        <header className="fixed pb-2 lg:py-0 lg:sticky left-0 right-0  top-0 z-50 bg-blue-500 ">
             <AlertNews />
             <HeaderNav />
         </header>
