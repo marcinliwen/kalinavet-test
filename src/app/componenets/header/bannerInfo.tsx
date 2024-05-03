b9ddf5bc5e024efe7f1f2a403b436a4e239d2989
m.liwen <liwen.marcin@gmail.com>
2024-05-03 23:47:35 +0200

    close

--- src/app/componenets/header/bannerInfo.tsx
+++ src/app/componenets/header/bannerInfo.tsx
@@ -7,8 +7,8 @@ export default function BannerInfo(){
     const t = useTranslations('AlertNews')
     return(
         <div className='container mx-auto gap-4  items-center py-1 text-white text-sm whitespace-nowrap text-right overflow-auto '>
-            {t('is_open')} {'9:30 - 13:30'}
-                  {/*   {t('today_closed')} */}
+           {/*  {t('is_open')} {'9:30 - 13:30'} */}
+                    {t('today_closed')}
                 </div>
     )
 }
