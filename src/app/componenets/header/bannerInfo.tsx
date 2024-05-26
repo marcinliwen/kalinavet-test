1e3d55c06fb3a66d3d83679cd2935fcc7af9534c
m.liwen <liwen.marcin@gmail.com>
2024-05-26 22:02:25 +0200

    top banner graphql

--- src/app/componenets/header/bannerInfo.tsx
+++ src/app/componenets/header/bannerInfo.tsx
@@ -1,14 +1,47 @@
-'use client'
 import { useTranslations } from "next-intl"
-import Link from "next/link"
+type OpenDays = {
+    openDays: string[],
+    displayOnTopBanner: boolean
+}
+async function getOpenDays() {
+    const { data } = await fetch('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cldqjnpm22vwp01uldwwx5ejk/master', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: `
+            query OpenDays {
+                openDays {
+                  openDays
+                  displayOnTopBanner
+                }
+              }`
+        }),
+    }).then((res) => res.json());
+    return data.openDays[0];
+}
+function isOpenToday(openDays : string[]){
+    const today = new Date;
+    const dateList = openDays.map(item => new Date(item).toLocaleDateString())
+    if(dateList.includes(today.toLocaleDateString())){
+        return true;
+    }
+    return false;
+}
 
+export default async function BannerInfo() {
+    const t = useTranslations('AlertNews');
+    const { openDays, displayOnTopBanner }: OpenDays = await getOpenDays();
 
-export default function BannerInfo(){
-    const t = useTranslations('AlertNews')
-    return(
+    if (!displayOnTopBanner) return;
+
+    const isOpen = isOpenToday(openDays)
+    return (
         <div className='container mx-auto gap-4  items-center py-1 text-white text-sm whitespace-nowrap text-right overflow-auto '>
-           {/*  {t('is_open')} {'9:30 - 13:30'} */}
-                    {t('today_closed')}
-                </div>
+            {isOpen ? `${t('is_open')} 9:30 - 13:30` : t('today_closed')}
+        </div>
     )
 }
+
+
